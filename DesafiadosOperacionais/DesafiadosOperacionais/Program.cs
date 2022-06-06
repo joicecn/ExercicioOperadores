4dafccf2913aa2964f98be38e0c54d50c81537e6
Joice Carla Nunes <joice.nunes@programmers.com.br>
2022-06-06 11:19:33 -0300

    Subindo Exercicio

---
+++ DesafiadosOperacionais/DesafiadosOperacionais/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Insira a nota final");
+int notaFinal = int.Parse(Console.ReadLine());
+
+if (notaFinal < 5)
+    Console.WriteLine("Você foi reprovado");
+
+else if (notaFinal > 6 || notaFinal < 7)
+    Console.WriteLine("Você esta de recuperação");
+
+else Console.WriteLine("Aluno aprovado");
+
+
+
+
